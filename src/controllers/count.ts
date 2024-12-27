fdd00046f2faaf9826c00314c249594218dc8d5a
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2024-12-27 19:01:22 -0300

    feat: adição no somatório da quantidade total de itens e a data atual

--- src/controllers/count.ts
+++ src/controllers/count.ts
@@ -6,11 +6,16 @@ export const getTotalSales = async (req: Request, res: Response) => {
         const totalSales = await prismaCilent.sale.aggregate({
             _sum: {
                 totalPrice: true,
+                quantity: true,
             },
         });
 
+        const currentDate = new Date();
+
         res.json({
             totalSales: totalSales._sum.totalPrice || 0,
+            totalProductsSold: totalSales._sum.quantity || 0,
+            currentDate: currentDate.toISOString(),
         });
     } catch (error) {
         console.error(error);
