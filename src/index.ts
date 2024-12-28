5dfae04502ac38051aeb86019a3924ad50d54bd4
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2024-12-28 18:25:18 -0300

    fix: ajuste de erro no cors

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import rootRouter from "./router";
 import { PrismaClient } from "@prisma/client";
 import { PORT } from "./secrets";
 import { errorMiddleware } from "./middlewares/errors";
+var cors = require('cors');
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/api", rootRouter);
 
 export const prismaCilent = new PrismaClient({
